cf6ecdd56f029b3c9d7da8bba21c168f5754e3e3
aditi <aditi@DESKTOP-343N0FS>
2020-11-08 08:48:32 +0700

    Upload Project Tugas

---
+++ src/if1/pkg10119016/latihan40/warnakepribadian2/IF110119016Latihan40WarnaKepribadian2.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119016.latihan40.warnakepribadian2;
+
+/**
+ *
+ * @author aditi
+ * NAMA              : Aditya Ilham Subagja
+ * KELAS             : IF1
+ * NIM               : 10119016
+ * Deskripsi Program : Program ini berisi program untuk menampilkan warna
+ * kepribadian berbasis objek
+ */
+public class IF110119016Latihan40WarnaKepribadian2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        WarnaKepribadian kepribadian = new WarnaKepribadian();
+        kepribadian.inputWarna();
+        kepribadian.inputNama();
+        kepribadian.cekKepribadian();
+    }
+    
+}
